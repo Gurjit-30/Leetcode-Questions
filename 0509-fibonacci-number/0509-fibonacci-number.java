0ffbddc497f86894170ee7069533b73a6813dddc
Gurjit-30 <143714757+Gurjit-30@users.noreply.github.com>
2026-02-03 11:25:42 +0530

    Time: 0 ms (100%), Space: 42 MB (82.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,20 @@
 class Solution {
-    public static int fib(int n){
+    public static int f(int n,int[] arr){
         if(n==0 || n==1){
             return n;
+
         }
-         return fib(n-1)+fib(n-2);
+        if(arr[n]!=-1){
+            return arr[n];
+        }
+        arr[n]=f(n-1,arr)+f(n-2,arr);
+        return arr[n];
+    }
+    public static int fib(int n){
+        int[] arr=new int[n+1];
+        Arrays.fill(arr,-1);
+        return f(n,arr);
+       
          
        
         
