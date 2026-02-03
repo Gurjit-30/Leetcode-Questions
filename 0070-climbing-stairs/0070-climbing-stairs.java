46983bea6543230c93517c0db62f40a42ea2c5ba
Gurjit-30 <143714757+Gurjit-30@users.noreply.github.com>
2026-02-03 12:09:08 +0530

    Time: 0 ms (100%), Space: 42 MB (83.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,29 @@
 class Solution {
-    public static int cs(int n,int[] arr){
-        if(n==0){
-            return 1;
-        }
-        if(n<0){
-            return 0;
-        }
-        if(arr[n]!=-1){
-            return arr[n];
-        }
-        arr[n]= cs(n-1,arr)+cs(n-2,arr);
-        return arr[n];
+    // public static int cs(int n,int[] arr){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(n<0){
+    //         return 0;
+    //     }
+    //     if(arr[n]!=-1){
+    //         return arr[n];
+    //     }
+    //     arr[n]= cs(n-1,arr)+cs(n-2,arr);
+    //     return arr[n];
 
-    }
+    // }
     public int climbStairs(int n) {
         int[] arr=new int[n+1];
         Arrays.fill(arr,-1);
-        return cs(n,arr);
-        
+        if(n==0 ||n==1){
+            return 1;
+        }
+       arr[0]=1;
+       arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 }
