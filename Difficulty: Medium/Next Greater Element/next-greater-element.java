9cc4ae059c3adee514f751c24876f1444a86693d
Gurjit-30 <143714757+Gurjit-30@users.noreply.github.com>
2026-02-27 13:05:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -4,26 +4,18 @@ class Solution {
         Stack<Integer>st=new Stack<>();
         ArrayList<Integer> res=new ArrayList<>();
         
-        st.push(arr[arr.length -1]);
-        res.add(-1);
-        for(int i=arr.length-2;i>=0;i--){
-        
-                while(!st.isEmpty() && arr[i]>=st.peek()){
-                       st.pop();
-                }
-                 if(st.isEmpty()){
-                res.add(-1);
-                 }else{
-                     res.add(st.peek());
-                     
-                 }
-             
-                
-                
-            st.push(arr[i]);
-            
-            
-        }
+       for(int i=arr.length-1;i>=0;i--){
+           while(!st.isEmpty() && arr[i]>=st.peek()){
+               st.pop();
+           }
+           if(st.isEmpty()){
+               res.add(-1);
+           }else{
+               res.add(st.peek());
+           }
+           st.push(arr[i]);
+       }
+       
          Collections.reverse(res);
         return res;
     }
